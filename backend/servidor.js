95688798b0b4886a24deeb259ac2556cbc64fd98
candelario02 <huamancarrioncande22@gmail.com>
2026-03-16 18:33:58 -0500

    fix: puerto dinamico y host 0.0.0.0 para render

--- backend/servidor.js
+++ backend/servidor.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-
 const authRutas = require('./src/rutas/authRutas');
 const adminRutas = require('./src/rutas/adminRutas');
 const publicoRutas = require('./src/rutas/publicoRutas');
@@ -11,20 +10,20 @@ const comercioRutas = require('./src/rutas/comercioRutas');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    // agregamos la url de la web
+    origin: ['http://localhost:5173', 'https://tu-frontend.vercel.app'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
 
+
 app.use(express.json());
 
-// registro de Rutas
+// registro rutas
 app.use('/api/auth', authRutas);
 app.use('/api/admin', adminRutas);
 app.use('/api/publico', publicoRutas);
 app.use('/api/comercio', comercioRutas);
-
-
 
 app.use((req, res) => {
     res.status(404).json({ 
@@ -33,11 +32,14 @@ app.use((req, res) => {
     });
 });
 
-const PORT = 5000;
 
-app.listen(PORT, () => {
+// puerto dinamico
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`--------------------------------------------------`);
     console.log(`🚀 SERVIDOR MUNICIPAL DE PACHACÁMAC`);
-    console.log(`📡 Endpoint: http://localhost:${PORT}`);
+    console.log(`📡 Puerto: ${PORT}`);
+    console.log(`🌍 Host: 0.0.0.0 (Aceptando conexiones externas)`);
     console.log(`--------------------------------------------------`);
 });
