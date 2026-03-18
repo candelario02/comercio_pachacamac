fe43912ee649fe622668cce5de811ffe7b32f955
candelario02 <huamancarrioncande22@gmail.com>
2026-03-18 07:47:56 -0500

    🌎 URL final de Render: Adios localhost

--- backend/src/rutas/adminRutas.js
+++ backend/src/rutas/adminRutas.js
@@ -6,14 +6,13 @@ const adminControlador = require('../controladores/adminControlador');
 // --- rutas administrativas
 
 router.get('/estadisticas', verificarAdmin, adminControlador.obtenerEstadisticas);
-//router.get('/solicitudes-pendientes', verificarAdmin, adminControlador.obtenerSolicitudesPendientes);
+router.get('/solicitudes-pendientes', verificarAdmin, adminControlador.obtenerSolicitudesPendientes);
 router.put('/solicitudes/:id/estado', verificarAdmin, adminControlador.actualizarEstado);
 router.get('/pagos-pendientes', verificarAdmin, adminControlador.obtenerPagosPendientes);
 router.put('/confirmar-pago/:id', verificarAdmin, adminControlador.confirmarPagoYFinalizar);
 router.get('/formalizados', verificarAdmin, adminControlador.obtenerFormalizados);
 
 router.put('/aprobar-tramite/:id', verificarAdmin, adminControlador.aprobarTramiteYGenerarDeuda);
-router.get('/solicitudes-pendientes', adminControlador.obtenerSolicitudesPendientes);
 
 router.get('/actividades', verificarAdmin, adminControlador.listarActividades);
 router.post('/gestionar-actividad', verificarAdmin, adminControlador.gestionarActividad);
