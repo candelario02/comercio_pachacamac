e91130dab6aeeb6d1618bba410130fbd4c2423a7
candelario02 <huamancarrioncande22@gmail.com>
2026-03-23 12:08:00 -0500

    jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,10 +30,9 @@ function App() {
           <Route path="/" element={<LayoutFondo><InicioPublico /></LayoutFondo>} />
           <Route path="/rubros-y-actividades" element={<LayoutFondo><ListaPublicaRubrosActividad /></LayoutFondo>} />
           <Route path="/ordenanzas" element={<LayoutFondo><Ordenanzas /></LayoutFondo>} />
-          <Route path="/categorias" element={<LayoutFondo><Categorias /></LayoutFondo>} />
+         
 <Route path="/definiciones" element={<LayoutFondo><Definiciones /></LayoutFondo>} />
-<Route path="/requisitos" element={<LayoutFondo><Requisitos /></LayoutFondo>} />
-<Route path="/vigencia" element={<LayoutFondo><Vigencia /></LayoutFondo>} />
+
           <Route path="/login" element={<Login />} />
           <Route path="/registro-solicitud" element={<LayoutFondo><SolicitudComerciante /></LayoutFondo>} />
           <Route path="/ordenanzas" element={<Ordenanzas />} />
