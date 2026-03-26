590f73664fbc240853a5b91e8760bf84b2e6bb35
candelario02 <huamancarrioncande22@gmail.com>
2026-03-26 13:08:11 -0500

    Optimización de imágenes: movidas a public y automatización de Slider con .map

--- frontend/src/componentes/LayoutFondo.jsx
+++ frontend/src/componentes/LayoutFondo.jsx
@@ -3,24 +3,39 @@ import Navbar from './Navbar';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
-import munipacha from '../assets/imagenes/imageslider/munipacha.jpeg';
-import santuariopacha from '../assets/imagenes/imageslider/santuariopacha.jpeg';
 import '../estilos/LayoutFondo.css';
 
 const LayoutFondo = ({ children }) => {
+    const imagenesSlider = [
+        "/img/santuariopacha.jpeg",
+        "/img/munipacha.jpeg"
+
+    ];
+
     return (
         <div className="layout-wrapper">
             <div className="fondo-slider">
-                <Swiper modules={[Autoplay]} slidesPerView={1} autoplay={{ delay: 4000 }} loop>
-                    <SwiperSlide><img src={santuariopacha} alt="fondo" /></SwiperSlide>
-                    <SwiperSlide><img src={munipacha} alt="fondo" /></SwiperSlide>
+                <Swiper 
+                    modules={[Autoplay]} 
+                    slidesPerView={1} 
+                    autoplay={{ delay: 4000 }} 
+                    loop
+                >
+                    {imagenesSlider.map((ruta, index) => (
+                        <SwiperSlide key={index}>
+                            <img src={ruta} alt={`fondo-pachacamac-${index}`} />
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
             </div>
+
             <Navbar />
+
             <div className="contenido-dinamico">
                 {children}
             </div>
         </div>
     );
 };
+
 export default LayoutFondo;
